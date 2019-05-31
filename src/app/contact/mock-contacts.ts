eb78cbbe30153601c053b9eb7465cb82d95b3596
Zicky78 <16zns01@gmail.com>
2019-05-30 20:55:53 -0700

    contact

---
+++ src/app/contact/mock-contacts.ts
@@ -0,0 +1,18 @@
+import { ContactInterface } from '../interfaces/contact-interface';
+
+export const CONTACTS: ContactInterface[] = [
+  {
+    id: 1,
+    name: 'Bob',
+    phone: '5551112233',
+    email: 'bob@bob.bob',
+    relation: 'Builder'
+  },
+  {
+    id: 2,
+    name: 'Smith',
+    phone: '1234567890',
+    email: 'smith@smith.smith',
+    relation: 'Smith'
+  }
+];
